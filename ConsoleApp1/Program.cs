f0dfc45345aa02e3e8a01b9feec9647233c1c424
Yugov Aleksey <aleksey.yugov.1992@gmail.com>
2021-05-06 14:31:20 +0500

    Добавьте файлы проекта.

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,136 @@
+﻿using System;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("\tРабота с массивами");
+            int[] A = new int[5];
+            for (int i = 0; i < A.Length; i++) // заполнение первого массива
+            {
+                Console.WriteLine("Введите " + (i + 1) + " элемент массива целочисленным значением");
+                A[i] = int.Parse(Console.ReadLine());
+            }
+            Console.WriteLine();
+            int number1, number2;
+            number1 = 3; // строки 
+            number2 = 4; // столбцы
+            double[,] B = new double[number1, number2];
+            Random rand = new Random();
+            rand.NextDouble();
+
+            for (int i = 0; i < number1; i++) // заполнение второго массива
+            {
+                for (int j = 0; j < number2; j++)
+                {
+                    B[i, j] = rand.NextDouble();
+                }
+            }
+
+            for (int i = 0; i < A.Length; i++) // вывод первого массива
+            {
+                Console.Write(A[i]);
+                Console.Write('\t');
+            }
+            Console.WriteLine();
+            Console.WriteLine();
+
+            for (int i = 0; i < number1; i++) // вывод второго массива
+            {
+                for (int j = 0; j < number2; j++)
+                {
+                    Console.Write(B[i, j]);
+                    Console.Write('\t');
+                }
+                Console.WriteLine();
+            }
+            Console.WriteLine();
+
+            double max = A[0];
+            double min = A[0];
+            double sum = 0;
+            double proizved = 1;
+            double sumchet = 0;
+            double sumnechet = 0;
+
+            for (int i = 0; i < A.Length; i++)
+            {
+                if (A[i]>max) // нахождение максимального элемента первого массива
+                {
+                    max = A[i];
+                }
+                if (A[i]<min) // нахождение минимального элемента первого массива
+                {
+                    min = A[i];
+                }
+                sum += A[i]; // нахождение суммы элементов первого массива
+                proizved *= A[i]; // нахождение произведения элементов первого массива
+                if (A[i] % 2 == 0) // нахождение суммы четных элементов первого массива
+                {
+                    sumchet += A[i];
+                }
+                if (A[i] % 2 != 0) // нахождение суммы нечетных элементов первого массива
+                {
+                    sumnechet += A[i];
+                }
+            }
+
+            Console.WriteLine("Сумма нечетных чисел массива_1 = " + sumnechet);
+            Console.WriteLine("Сумма четных чисел массива_1 = " + sumchet);
+            Console.WriteLine("Произведение массива_1 = " + proizved);
+            Console.WriteLine("Сумма массива_1 = " + sum);
+            Console.WriteLine("Минимальное значение массива_1 = " + min);
+            Console.WriteLine("Максимальный элемент массива_1 = " + max);
+            Console.WriteLine("====================================================");
+            
+            sum = 0; // обнуление данных для второго массива
+            proizved = 1;
+            sumchet = 0;
+            sumnechet = 0;
+            max = B[0, 0];
+            min = B[0, 0];
+            double sumnechetnumber2 = 0;
+
+            for (int i = 0; i < number1; i++)
+            {
+                for (int j = 0; j < number2; j++)
+                {
+                    if (B[i,j]>max) // нахождение максимального элемента второго массива
+                    {
+                        max = B[i, j];
+                    }
+                    if (B[i, j] < max) // нахождение минимального элемента второго массива
+                    {
+                        min = B[i, j];
+                    }
+                    sum += B[i, j]; // нахождение суммы элементов второго массива
+                    proizved *= B[i,j];
+                    if (B[i,j] % 2 == 0) // нахождение суммы четных элементов второго массива
+                    {
+                        sumchet += A[i];
+                    }
+                    if (A[i] % 2 != 0) // нахождение суммы нечетных элементов второго массива
+                    {
+                        sumnechet += B[i,j];
+                    }
+                    if (j % 2 != 0)
+                    {
+                        sumnechetnumber2 += B[i, j];
+                    }
+                }
+                
+            }
+
+            Console.WriteLine("Сумма нечетных чисел массива_2 = " + sumnechet);
+            Console.WriteLine("Сумма четных чисел массива_2 = " + sumchet);
+            Console.WriteLine("Произведение массива_2 = " + proizved);
+            Console.WriteLine("Сумма массива_2 = " + sum);
+            Console.WriteLine("Минимальное значение массива_2 = " + min);
+            Console.WriteLine("Максимальный элемент массива_2 = " + max);
+            Console.WriteLine("Сумма элементов в нечетных столбцах массива_2 = " + sumnechetnumber2);
+        }
+        
+    }
+}
---
+++ ConsoleApp2/Program.cs
@@ -0,0 +1,195 @@
+﻿using System;
+
+
+namespace ConsoleApp2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Random rand = new Random();
+            int[] arr1 = new int[5];
+            int[] arr2 = new int[5];
+
+            for (int j = 0; j < arr1.Length; j++) //заполнение первого массива
+            {
+                arr1[j] = rand.Next() % 10;
+            }
+
+            for (int j = 0; j < arr2.Length; j++) //заполнение второго массива
+            {
+                arr2[j] = rand.Next() % 10;
+            }
+
+            Console.WriteLine("Первый массив:");
+            foreach (var item in arr1) // вывод первого массива
+            {
+                Console.Write(item + "\t");
+            }
+            Console.WriteLine();
+
+            Console.WriteLine("Второй массив:");
+            foreach (var item in arr2) // вывод второго массива
+            {
+                Console.Write(item + "\t");
+            }
+            Console.WriteLine();
+
+            // проверяем на повторения элементы и записываем в третий массив
+            int k = 0;
+            for (int i = 0; i < arr1.Length; i++)
+            {
+                for (int j = 0; j < arr2.Length; j++)
+                {
+                    if(arr1[i]==arr2[j])
+                    {                        
+                        k++;
+                    }
+                }
+            }
+            int[] arr3 = new int[k];
+            k = 0;
+            for (int i = 0; i < arr1.Length; i++)
+            {
+                for (int j = 0; j < arr2.Length; j++)
+                {
+                    if (arr1[i] == arr2[j])
+                    {                        
+                        arr3[k] = arr1[i];
+                        k++;                        
+                    }
+                }
+            }
+
+            // выводим третий массив
+            Console.WriteLine("Третий массив содержит повторения значиний первых двух массивов: ");
+            if (arr3.Length ==0)
+            {
+                Console.WriteLine("Повторений нет!");
+            }
+            else 
+            {
+                foreach (var item in arr3)
+                {
+                    Console.Write(item + "\t");
+                }
+                Console.WriteLine();
+            }
+            Console.WriteLine();
+
+            // проверка на палиндром
+            Console.WriteLine("Введите строку, для проверки является ли она полиндромом");
+            string str = Console.ReadLine();
+            char[] stroka2 = new char[str.Length];
+            char[] stroka1 = new char[str.Length];
+            for (int i = str.Length,j = 0; i > 0; i--,j++)
+            {
+                stroka1[j] = str[i-1];
+                Console.Write(stroka1[j]);
+            }
+            Console.WriteLine();
+            for (int i = 0; i < str.Length; i++)
+            {
+                stroka2[i] = str[i];
+                Console.Write(stroka2[i]);
+            }
+            Console.WriteLine();
+            int p = 1;
+            for (int i = 0; i < str.Length; i++)
+            {
+                if (stroka1[i]==stroka2[i])
+                {
+
+                }    
+                else
+                {
+                    p = 0;
+                }
+                
+            }
+            if (p == 0)
+            {
+                Console.WriteLine("НЕ является палиндромом");
+            }
+            else
+            {
+                Console.WriteLine("является палиндромом");
+            }
+            Console.WriteLine();
+            //подсчет слов в строке
+            int slovo = 0;
+            for (int i = 0; i < str.Length; i++)
+            {
+                if (stroka2[i]==' ')
+                {
+                    if (stroka2[i+1]!=' ')
+                    {
+                        slovo++;
+                    }
+                }
+            }
+            Console.WriteLine("В строке " + (slovo+1) + " слов(а)");
+            Console.WriteLine();
+
+            //подсчет элементов между минимальным и максимальным элементами массива
+            Random rand2 = new Random();
+            int[,] array = new int[5,5];
+            for (int i = 0; i < array.GetLength(0); i++)
+            {
+                for (int j = 0; j < array.GetLength(1); j++)
+                {
+                    array[i, j] = rand2.Next(-100,100);
+                }
+            }
+            int max = array[0, 0];
+            int min = array[0, 0];
+            for (int i = 0; i < array.GetLength(0); i++)
+            {
+                for (int j = 0; j < array.GetLength(1); j++)
+                {
+                    Console.Write(array[i, j] + "\t");
+                    if (array[i, j] > max) max = array[i, j];
+                    if (array[i, j] < min) min = array[i, j];
+                }
+                Console.WriteLine();
+            }
+            Console.WriteLine();
+            Console.WriteLine("Минимальное значение = " + min);
+            Console.WriteLine("Максимальное значение = " + max);
+            int minindex1 = 0;
+            int minindex2 = 0;
+            int maxindex1 = 0;
+            int maxindex2 = 0;
+            int summ = 0;
+
+            for (int i = 0; i < array.GetLength(0); i++)
+            {
+                for (int j = 0; j < array.GetLength(1); j++)
+                {
+                    if (array[i,j]<array[minindex1,minindex2])
+                    {
+                        minindex1 = i; // нахождение индексов у минимального и максимального
+                        minindex2 = j;
+                    }
+                    else if (array[i, j] > array[minindex1, minindex2])
+                    {
+                        maxindex1 = i;
+                        maxindex2 = j;
+                    }
+                }
+            }
+            
+            for (int i = minindex1; i <= maxindex1; i++) //подсчет суммы элементов от минимального до максимального
+            {
+                for (int j = minindex2; j <= maxindex2; j++)
+                {
+                    summ += array[i, j];
+                }
+            }
+            summ -= (min + max);
+            
+            Console.WriteLine("Сумма элементов между минимальным и максимальным элементами или между максимальным и минимальным = " + summ);
+            Console.WriteLine();
+        }
+    }
+}
